4d40731540b82d1c00f378acd16ba49607062072
Chris Chen <cschen13@gmail.com>
2017-01-21 13:47:06 -0500

    Sort the table

--- app/kitchen/kitchen.controller.js
+++ app/kitchen/kitchen.controller.js
@@ -2,5 +2,15 @@
     angular.module("chefCurry")
     .controller("KitchenController",["$scope", "$http", "items", function($scope, $http, items){
         $scope.items = items;
+        $scope.sortType = 'ingredientName';
+        $scope.sortReverse = false;
+        $scope.sortTable = function(type) {
+        	if ($scope.sortType == type) {
+        		$scope.sortReverse = !$scope.sortReverse;
+        	} else {
+				$scope.sortType = type;
+        		$scope.sortReverse = false;
+        	}
+        }
     }]);
 }());
